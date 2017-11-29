5a6f85ffea42f14585c60052b0c5b2848956f955
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2017-11-29 19:04:06 +0100

    improve install script

--- install-debian.sh
+++ install-debian.sh
@@ -1,7 +1,17 @@
-pip install virtualenv
-sudo apt-get install yarn
+#!/bin/bash
+
+echo -e "\e[1m\e[34mBienvenue sur le script d'installation de l'aligneur de bitextes"
+echo -e "Ce script est destiné aux distributions Ubuntu-like. \e[31mUn compte GitHub est nécessaire.\e[0m"
+read -p "Adresse mail du compte GitHub : " mail
+read -p "Prénom Nom : " name
+
+### INSTALL ###
+echo -e "\e[1m\e[34mInstallation des dépendances...\e[0m"
+sudo -E apt-get update
+sudo -E apt-get install -y python-pip yarn # atom qtcreator
 
 cd back/
+pip install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt

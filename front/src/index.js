f613bb0c79f98089acb284e094f3026561aa00a5
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2018-02-10 15:04:06 +0000

    prod url

--- front/src/index.js
+++ front/src/index.js
@@ -12,7 +12,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { BrowserRouter } from 'react-router-dom'
 
-const httpLink = new BatchHttpLink({ uri: "http://localhost:8000/gql/" });
+const httpLink = new BatchHttpLink({ uri: "http://alignment.fdesousa.fr:8000/gql/" });
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()

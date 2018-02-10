f6b4f45b604227bdae84a54d27eea1295ebea4a3
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2018-02-10 17:16:33 +0100

    WIP

--- back/bitext_matching/settings_prod.py
+++ back/bitext_matching/settings_prod.py
@@ -1,5 +1,5 @@
 from .settings import *
-from .env import SECRET_KEY
+from .env_settings import *
 
 DEBUG = False
 
@@ -16,3 +16,15 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'http://alignment.fdesousa.fr'  # Allow only the React frontend to send graphQL queries
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_NAME,                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASSWORD,
+        'HOST': 'localhost',             # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}

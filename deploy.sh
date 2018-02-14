e58a0715e11da518abe2108e42e616baba98f36f
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2018-02-15 00:14:32 +0100

    add ENV VAR to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ pip3 install -r requirements_prod.txt
 python manage.py migrate
 
 python manage.py collectstatic
+export DJANGO_SETTINGS_MODULE=bitext_matching.settings_prod
 python manage.py runmodwsgi
